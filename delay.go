8b995f09fb27f489870e6f4685534b30bdce61a9
Deleplace <deleplace2015@gmail.com>
2020-12-17 15:06:08 +0100

    Header Option: let the client set the desired durations.

--- delay.go
+++ delay.go
@@ -21,9 +21,9 @@ func Delay(h http.HandlerFunc, opts ...Option) http.HandlerFunc {
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		cfg.before()
+		cfg.before(w, r)
 		h(w, r)
-		cfg.after()
+		cfg.after(w, r)
 	}
 }
 
@@ -32,17 +32,30 @@ func Delay(h http.HandlerFunc, opts ...Option) http.HandlerFunc {
 type config struct {
 	fixedDurationBefore time.Duration
 	fixedDurationAfter  time.Duration
+	headerPrefix        string
 }
 
 // Option configures the behavior of the delayed handler.
 type Option func(*config)
 
-func (cfg *config) before() {
-	time.Sleep(cfg.fixedDurationBefore)
+func (cfg *config) before(w http.ResponseWriter, r *http.Request) {
+	var d time.Duration
+	if cfg.headerPrefix != "" {
+		d, _ = readHeaderDuration(r, cfg.headerPrefix+"-before")
+	} else {
+		d = cfg.fixedDurationBefore
+	}
+	time.Sleep(d)
 }
 
-func (cfg *config) after() {
-	time.Sleep(cfg.fixedDurationAfter)
+func (cfg *config) after(w http.ResponseWriter, r *http.Request) {
+	var d time.Duration
+	if cfg.headerPrefix != "" {
+		d, _ = readHeaderDuration(r, cfg.headerPrefix+"-after")
+	} else {
+		d = cfg.fixedDurationAfter
+	}
+	time.Sleep(d)
 }
 
 // Fixed sets how long to pause before and after the wrapped HandlerFunc is executed.
@@ -51,4 +64,35 @@ func Fixed(before, after time.Duration) Option {
 		cfg.fixedDurationBefore = before
 		cfg.fixedDurationAfter = after
 	}
+}
+
+// Header sets names of request headers that the client may set to control the
+// delay durations.
+//
+// E.g. "delay" for inspecting "delay-before" and "delay-after"
+// The expected format for the header values is a parsable duration, e.g.
+// "delay-before: 300ms", "delay-after: 1.5s", etc.
+//
+// When Header is used and headers are not set by the client for a given request, then
+// no delays are applied to this request.
+//
+// When Header is used, we consider that the client is in charge of deciding the delay
+// durations, and any use of Fixed would be ignored. Header and Fixed should not be
+// used at the same time.
+func Header(prefix string) Option {
+	return func(cfg *config) {
+		cfg.headerPrefix = prefix
+	}
+}
+
+func readHeaderDuration(r *http.Request, name string) (time.Duration, bool) {
+	value := r.Header.Get(name)
+	if value == "" {
+		return 0, false
+	}
+	d, err := time.ParseDuration(value)
+	if err != nil {
+		return 0, false
+	}
+	return d, true
 }
--- delay_test.go
+++ delay_test.go
@@ -22,57 +22,115 @@ func TestDelayNoArguments(t *testing.T) {
 	delayedHandler := Delay(helloWorld)
 	expectedExtraLatency := 1 * time.Second
 
-	messageBytes, responseTime := call(t, delayedHandler)
+	messageBytes, responseTime := call(t, delayedHandler, nil)
 
 	testOuput(t, messageBytes, "Hello world\n")
 	testDurationWithTolerance(t, responseTime, expectedExtraLatency)
 }
 
 // h = Delay(h, Fixed(d, 0))
-func TestDelayBefore(t *testing.T) {
+func TestFixedDelayBefore(t *testing.T) {
 	const extraLatency = 400 * time.Millisecond
 	delayedHandler := Delay(helloWorld, Fixed(extraLatency, 0))
 
-	messageBytes, responseTime := call(t, delayedHandler)
+	messageBytes, responseTime := call(t, delayedHandler, nil)
 
 	testOuput(t, messageBytes, "Hello world\n")
 	testDurationWithTolerance(t, responseTime, extraLatency)
 }
 
 // h = Delay(h, Fixed(0, d))
-func TestDelayAfter(t *testing.T) {
+func TestFixedDelayAfter(t *testing.T) {
 	const extraLatency = 400 * time.Millisecond
 	delayedHandler := Delay(helloWorld, Fixed(0, extraLatency))
 
-	messageBytes, responseTime := call(t, delayedHandler)
+	messageBytes, responseTime := call(t, delayedHandler, nil)
 
 	testOuput(t, messageBytes, "Hello world\n")
 	testDurationWithTolerance(t, responseTime, extraLatency)
 }
 
 // h = Delay(h, Fixed(d1, d2))
-func TestDelayBeforeAfter(t *testing.T) {
+func TestFixedDelayBeforeAfter(t *testing.T) {
 	const extraLatencyBefore = 300 * time.Millisecond
 	const extraLatencyAfter = 700 * time.Millisecond
 	delayedHandler := Delay(helloWorld, Fixed(extraLatencyBefore, extraLatencyAfter))
 
-	messageBytes, responseTime := call(t, delayedHandler)
+	messageBytes, responseTime := call(t, delayedHandler, nil)
 
 	testOuput(t, messageBytes, "Hello world\n")
 	testDurationWithTolerance(t, responseTime, extraLatencyBefore+extraLatencyAfter)
 }
 
-// Helper: call the handler while measuring response time.
-func call(t *testing.T, h http.HandlerFunc) ([]byte, time.Duration) {
+// h = Delay(h, Header(prefix))
+func TestHeaderDelayBefore(t *testing.T) {
+	const extraLatency = 400 * time.Millisecond
+	const extraLatencyString = "400ms"
+	const prefix = "delay"
+
+	delayedHandler := Delay(helloWorld, Header(prefix))
+
+	headers := http.Header{
+		"delay-before": []string{extraLatencyString},
+	}
+	messageBytes, responseTime := call(t, delayedHandler, headers)
+
+	testOuput(t, messageBytes, "Hello world\n")
+	testDurationWithTolerance(t, responseTime, extraLatency)
+}
+
+// h = Delay(h, Header(prefix))
+func TestHeaderDelayAfter(t *testing.T) {
+	const extraLatency = 400 * time.Millisecond
+	const extraLatencyString = "400ms"
+	const prefix = "delay"
+
+	delayedHandler := Delay(helloWorld, Header(prefix))
+
+	headers := http.Header{
+		"delay-after": []string{extraLatencyString},
+	}
+	messageBytes, responseTime := call(t, delayedHandler, headers)
+
+	testOuput(t, messageBytes, "Hello world\n")
+	testDurationWithTolerance(t, responseTime, extraLatency)
+}
+
+// h = Delay(h, Header(prefix))
+func TestHeaderBeforeDelayAfter(t *testing.T) {
+	const extraLatencyBefore = 300 * time.Millisecond
+	const extraLatencyBeforeString = "300ms"
+	const extraLatencyAfter = 700 * time.Millisecond
+	const extraLatencyAfterString = "700ms"
+	const prefix = "delay"
+
+	delayedHandler := Delay(helloWorld, Header(prefix))
+
+	headers := http.Header{
+		"delay-before": []string{extraLatencyBeforeString},
+		"delay-after":  []string{extraLatencyAfterString},
+	}
+	messageBytes, responseTime := call(t, delayedHandler, headers)
+
+	testOuput(t, messageBytes, "Hello world\n")
+	testDurationWithTolerance(t, responseTime, extraLatencyBefore+extraLatencyAfter)
+}
+
+// Helper: call the handler with specific request headers, while measuring response time.
+func call(t *testing.T, h http.HandlerFunc, headers http.Header) ([]byte, time.Duration) {
 	s := httptest.NewServer(h)
 	defer s.Close()
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", s.URL, nil)
+	req.Header = headers
 
 	var res *http.Response
 	var err error
 	// This is the response time from the client POV, so it is slightly
 	// larger than the server-POV service time.
 	responseTime := clock(func() {
-		res, err = http.Get(s.URL)
+		res, err = client.Do(req)
 	})
 	if err != nil {
 		t.Fatal(err)
@@ -101,10 +159,10 @@ func testOuput(t *testing.T, messageBytes []byte, expected string) {
 
 // Helper: check that elapsed time seem cromulent.
 func testDurationWithTolerance(t *testing.T, observed, expected time.Duration) {
-	if observed < expected-100*time.Millisecond {
+	if observed < expected-50*time.Millisecond {
 		t.Errorf("Response time too short: %v", observed)
 	}
-	if observed > expected+600*time.Millisecond {
+	if observed > expected+100*time.Millisecond {
 		t.Errorf("Response time too long: %v", observed)
 	}
 }
