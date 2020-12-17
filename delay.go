d590ba6da4e52348d8561b283b0fe76b6ae72e22
Deleplace <deleplace2015@gmail.com>
2020-12-17 13:01:02 +0100

    Functional options. Not ready for release yet.

--- delay.go
+++ delay.go
@@ -5,19 +5,39 @@ import (
 	"time"
 )
 
-// Delay adds an artificial 2s of latency at the start of the HandlerFunc.
+// Delay adds an artificial latency at the start of the HandlerFunc.
 //
 // Usage: myHandlerFunc = slowdown.Delay(myHandlerFunc)
 //
 // The call to Delay may be chained with other middleware when building a handler.
-func Delay(h http.HandlerFunc, cfg Config) http.HandlerFunc {
+func Delay(h http.HandlerFunc, opts ...Option) http.HandlerFunc {
+	// Default config values
+	cfg := config{
+		duration: 2 * time.Second,
+	}
+	// Apply options
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(cfg.Duration)
+		time.Sleep(cfg.duration)
 		h(w, r)
 	}
 }
 
-// Config exposes the parameters used for wrapping a HandlerFunc with Delay.
-type Config struct {
-	Duration time.Duration
+// config holds the parameters used for delaying a HandlerFunc.
+// It is not exported. Users must use exported Options and Option providers instead.
+type config struct {
+	duration time.Duration
+}
+
+// Option configures the behavior of the delayed handler.
+type Option func(*config)
+
+// Duration sets how long to delay the wrapped HandlerFunc.
+func Duration(d time.Duration) Option {
+	return func(cfg *config) {
+		cfg.duration = d
+	}
 }
--- delay_test.go
+++ delay_test.go
@@ -10,21 +10,59 @@ import (
 	"time"
 )
 
-func TestDelay(t *testing.T) {
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello world")
-	}
-	const extraLatency = 400 * time.Millisecond
-	delayedHandler := Delay(handler, Config{
-		Duration: extraLatency,
-	})
+// helloWorld is a trivial HandlerFunc. It takes very little time to execute.
+var helloWorld = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello world")
+}
 
-	s := httptest.NewServer(delayedHandler)
+// h = Delay(h)
+func TestDelayNoArguments(t *testing.T) {
+	delayedHandler := Delay(helloWorld)
+	expectedExtraLatency := 2 * time.Second
+
+	messageBytes, responseTime := call(t, delayedHandler)
+	// Test output
+	if expected, actual := "Hello world\n", string(messageBytes); actual != expected {
+		t.Errorf("Expected %q, got %q", expected, actual)
+	}
+	// Test duration, with some tolerance
+	if responseTime < expectedExtraLatency-100*time.Millisecond {
+		t.Errorf("Response time too short: %v", responseTime)
+	}
+	if responseTime > expectedExtraLatency+600*time.Millisecond {
+		t.Errorf("Response time too long: %v", responseTime)
+	}
+}
+
+// h = Delay(h, Duration(d))
+func TestDelay400ms(t *testing.T) {
+	const extraLatency = 400 * time.Millisecond
+	delayedHandler := Delay(helloWorld, Duration(extraLatency))
+
+	messageBytes, responseTime := call(t, delayedHandler)
+	// Test output
+	if expected, actual := "Hello world\n", string(messageBytes); actual != expected {
+		t.Errorf("Expected %q, got %q", expected, actual)
+	}
+	// Test duration, with some tolerance
+	if responseTime < extraLatency-100*time.Millisecond {
+		t.Errorf("Response time too short: %v", responseTime)
+	}
+	if responseTime > extraLatency+600*time.Millisecond {
+		t.Errorf("Response time too long: %v", responseTime)
+	}
+}
+
+// Helper: call the handler while measuring response time.
+func call(t *testing.T, h http.HandlerFunc) ([]byte, time.Duration) {
+	s := httptest.NewServer(h)
 	defer s.Close()
 
 	var res *http.Response
 	var err error
-	totalResponseTime := clock(func() {
+	// This is the response time from the client POV, so it is slightly
+	// larger than the server-POV service time.
+	responseTime := clock(func() {
 		res, err = http.Get(s.URL)
 	})
 	if err != nil {
@@ -35,16 +73,7 @@ func TestDelay(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	if expected, actual := "Hello world\n", string(messageBytes); actual != expected {
-		t.Errorf("Expected %q, got %q", expected, actual)
-	}
-	// Test duration, with some tolerance
-	if totalResponseTime < extraLatency-100*time.Millisecond {
-		t.Errorf("Response time too short: %v", totalResponseTime)
-	}
-	if totalResponseTime > extraLatency+600*time.Millisecond {
-		t.Errorf("Response time too long: %v", totalResponseTime)
-	}
+	return messageBytes, responseTime
 }
 
 // Helper: executes f and return how long it took.
