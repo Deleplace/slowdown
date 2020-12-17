a0c5dd166f3351427b0b8d3a3925ecaecfa32770
Deleplace <deleplace2015@gmail.com>
2020-12-17 12:19:35 +0100

    Test the duration of the delayed handler call.

--- delay_test.go
+++ delay_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestDelay(t *testing.T) {
@@ -19,7 +20,11 @@ func TestDelay(t *testing.T) {
 	s := httptest.NewServer(delayedHandler)
 	defer s.Close()
 
-	res, err := http.Get(s.URL)
+	var res *http.Response
+	var err error
+	totalResponseTime := clock(func() {
+		res, err = http.Get(s.URL)
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -31,4 +36,18 @@ func TestDelay(t *testing.T) {
 	if expected, actual := "Hello world\n", string(messageBytes); actual != expected {
 		t.Errorf("Expected %q, got %q", expected, actual)
 	}
+	// Test duration, with some tolerance
+	if totalResponseTime < 1900*time.Millisecond {
+		t.Errorf("Response time too short: %v", totalResponseTime)
+	}
+	if totalResponseTime > 2600*time.Millisecond {
+		t.Errorf("Response time too long: %v", totalResponseTime)
+	}
+}
+
+// Helper: executes f and return how long it took.
+func clock(f func()) time.Duration {
+	t := time.Now()
+	f()
+	return time.Since(t)
 }
